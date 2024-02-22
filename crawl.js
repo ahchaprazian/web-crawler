de883f9987f2354b91590fa5388f081935bb51e0
Alexan Chaprazian <ahchaprazian@gmail.com>
2024-02-22 12:34:54 -0500

    added new tests and new function to crawl.js

--- crawl.js
+++ crawl.js
@@ -1,3 +1,5 @@
+const { JSDOM } = require('jsdom')
+
 function normalizeURL(url) {
     const urlObj = new URL(url)
     let normalizedPath = `${urlObj.host}${urlObj.pathname}`
@@ -8,6 +10,32 @@ function normalizeURL(url) {
     return normalizedPath
 }
 
+function getURLsFromHTML(htmlBody, baseUrl) {
+    const urls = []
+    const dom = new JSDOM(htmlBody)
+    const elements = dom.window._document.querySelectorAll('a')
+
+    for (element of elements) {
+        if (element.href.slice(0, 1) === '/') {
+            try {
+                urls.push(new URL(element.href, baseUrl).href)
+            } catch (error) {
+                console.log(`${error.message}: ${element.href}`)
+            }
+        } else {
+            try {
+                urls.push(new URL(element.href).href)
+            } catch (error) {
+                console.log(`${error.message}: ${element.href}`)
+            }
+        }     
+    }
+    return urls
+}
+
+//getURLsFromHTML('<html><body><a href="https://blog.boot.dev"><span>Boot.dev></span></a></body></html>','https://blog.boot.dev')
+
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
